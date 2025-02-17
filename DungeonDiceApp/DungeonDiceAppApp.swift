ba95224e767b76a98b3d811fe953f7c701995a59
Kkenize <Kkenizez@gmail.com>
2025-02-17 11:13:49 -0500

    Initial Commit

---
+++ DungeonDiceApp/DungeonDiceAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDiceAppApp.swift
+//  DungeonDiceApp
+//
+//  Created by Zhejun Zhang on 2/17/25.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDiceAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
