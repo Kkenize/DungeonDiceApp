b8fb3d04395ee4feb76c8519cd7d1d37b3da40ae
Kkenize <Kkenizez@gmail.com>
2025-02-17 15:01:43 -0500

    DungeonDice Almost completed!

--- DungeonDiceApp/ContentView.swift
+++ DungeonDiceApp/ContentView.swift
@@ -8,12 +8,98 @@
 import SwiftUI
 
 struct ContentView: View {
+    enum Dice: Int, CaseIterable {
+        case four = 4
+        case six = 6
+        case eight = 8
+        case ten = 10
+        case twelve = 12
+        case twenty = 20
+        case hundred = 100
+        
+        func roll() -> Int {
+            return Int.random(in: 1...self.rawValue)
+        }
+    }
+    
+    @State private var message = ""
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Dungeon Dice")
+                .font(.largeTitle)
+                .fontWeight(.black)
+                .foregroundColor(.red)
+            
+            Spacer()
+            
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.medium)
+                .frame(height: 150)
+                .minimumScaleFactor(0.5)
+                .multilineTextAlignment(.center)
+            
+            Spacer()
+            
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 102))]) {
+                Group {
+                    ForEach(Dice.allCases, id: \.self) { dice in
+                        Button("\(dice.rawValue)-sided") {
+                            message = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
+                        }
+                        .buttonStyle(.borderedProminent)
+                        .tint(.red)
+                    }
+                }
+                
+            }
+            
+            //            Group {
+            //                HStack {
+            //                    Button("\(Dice.four.rawValue)-sided") {
+            //                        message = "You rolled a \(Dice.four.roll()) on a \(Dice.four.rawValue)-sided dice"
+            //                    }
+            //
+            //                    Spacer()
+            //
+            //                    Button("\(Dice.six.rawValue)-sided") {
+            //                        message = "You rolled a \(Dice.six.roll()) on a \(Dice.six.rawValue)-sided dice"
+            //                    }
+            //
+            //                    Spacer()
+            //
+            //                    Button("\(Dice.eight.rawValue)-sided") {
+            //                        message = "You rolled a \(Dice.eight.roll()) on a \(Dice.eight.rawValue)-sided dice"
+            //                    }
+            //                }
+            //                .buttonStyle(.borderedProminent)
+            //                .tint(.red)
+            //                HStack {
+            //                    Button("\(Dice.ten.rawValue)-sided") {
+            //                        message = "You rolled a \(Dice.ten.roll()) on a \(Dice.ten.rawValue)-sided dice"
+            //                    }
+            //
+            //                    Spacer()
+            //
+            //                    Button("\(Dice.twelve.rawValue)-sided") {
+            //                        message = "You rolled a \(Dice.twelve.roll()) on a \(Dice.twelve.rawValue)-sided dice"
+            //                    }
+            //
+            //                    Spacer()
+            //
+            //                    Button("\(Dice.twenty.rawValue)-sided") {
+            //                        message = "You rolled a \(Dice.twenty.roll()) on a \(Dice.twenty.rawValue)-sided dice"
+            //                    }
+            //                }
+            //                .buttonStyle(.borderedProminent)
+            //                .tint(.red)
+            //                Button("\(Dice.hundred.rawValue)-sided") {
+            //                    message = "You rolled a \(Dice.eight.roll()) on a \(Dice.hundred.rawValue)-sided dice"
+            //                }
+            //                .buttonStyle(.borderedProminent)
+            //                .tint(.red)
+            //            }
         }
         .padding()
     }
@@ -22,3 +108,4 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
