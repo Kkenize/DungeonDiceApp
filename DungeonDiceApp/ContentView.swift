ac048eb299f0955ee78668a9f1574690764a8b2e
Kkenize <Kkenizez@gmail.com>
2025-03-07 19:43:08 -0500

    Completed DungeonDiceApp!

--- DungeonDiceApp/ContentView.swift
+++ DungeonDiceApp/ContentView.swift
@@ -8,7 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
-    enum Dice: Int, CaseIterable {
+    enum Dice: Int, CaseIterable, Identifiable {
+        
         case four = 4
         case six = 6
         case eight = 8
@@ -17,12 +18,22 @@ struct ContentView: View {
         case twenty = 20
         case hundred = 100
         
+        var id: Int {
+            rawValue
+        }
+        
+        var description: String {
+            "\(rawValue)-sided"
+        }
+        
         func roll() -> Int {
             return Int.random(in: 1...self.rawValue)
         }
     }
     
     @State private var message = ""
+    @State private var animationTrigger = false
+    @State private var isDoneAnimating = true
     
     var body: some View {
         VStack {
@@ -37,16 +48,33 @@ struct ContentView: View {
                 .font(.largeTitle)
                 .fontWeight(.medium)
                 .frame(height: 150)
+//                .scaleEffect(isDoneAnimating ? 1.0 : 0.6)
+//                .opacity(isDoneAnimating ? 1.0 : 0.2)
+                .rotation3DEffect(isDoneAnimating ? .degrees(360): .degrees(0), axis: (x: 1.0, y: 0.0, z: 0.0))
                 .minimumScaleFactor(0.5)
                 .multilineTextAlignment(.center)
+//                .animation(.default, value: animationTrigger)
+//                .onChange(of: animationTrigger) {
+//                    isDoneAnimating = false
+//                    withAnimation(.spring(response: 0.3, dampingFraction: 0.4)){
+//                        isDoneAnimating = true
+//                    }
+//                }
+                .onChange(of: animationTrigger) {
+                    isDoneAnimating = false
+                    withAnimation(.interpolatingSpring(duration: 0.6, bounce: 0.4)){
+                        isDoneAnimating = true
+                    }
+                }
             
             Spacer()
             
             LazyVGrid(columns: [GridItem(.adaptive(minimum: 102))]) {
                 Group {
-                    ForEach(Dice.allCases, id: \.self) { dice in
-                        Button("\(dice.rawValue)-sided") {
+                    ForEach(Dice.allCases) { dice in
+                        Button(dice.description) {
                             message = "You rolled a \(dice.roll()) on a \(dice.rawValue)-sided dice"
+                            animationTrigger.toggle()
                         }
                         .buttonStyle(.borderedProminent)
                         .tint(.red)
